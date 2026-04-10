406a6663f9b95f4df0f129aabc1e8d6b78171a8d
Hoang Minh Ngo <mngo12332@gmail.com>
2026-04-10 02:25:10 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  allowedDevOrigins: ["*.use.devtunnels.ms", "*.shares.zrok.io"],
 };
 
 export default nextConfig;
